7d94d14ee45221c1a6fdf67d127adfc33338d353
Joaquin Florencio <Jrr232@gmail.com>
2025-11-19 21:40:01 -0500

    first commit

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,21 @@
+
+export default function HomePage() {
+    return (
+
+        <div className="jumbotron jumbotron-fluid" id="homepage-hero">
+            <div className="container" id="homepage-hero-text">
+                <div className="overlay"> {/* darkening overlay */}</div>
+                <h1 className="display-4">Help Pass Ryders Bill</h1>
+                <p className="lead">Our city has always led on progressive, compassionate change — let’s do the same for the horses who can’t speak for themselves.</p>
+                <p className="lead">
+                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </p>
+
+            </div>
+
+        </div>
+
+
+
+    );
+}
